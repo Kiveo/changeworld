757c4354d6d54afbfa9103f2a1d63424d1022fc8
unknown <mavericktesini@gmail.com>
2018-11-15 02:05:52 -0500

    create idea spec

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+
+RSpec.describe Idea, type: :model do 
+  it "has a name" do 
+    idea  = Idea.create!(name: "My Awesome Idea Name") #instantiate an instance
+    expect(idea.name).to eq("My Awesome Idea Name") #expect the arg to exist and be the assigned name
+  end
+end 
