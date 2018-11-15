736757ca3f23d88f565027de9b6b344fa6b59a1a
unknown <mavericktesini@gmail.com>
2018-11-15 01:53:14 -0500

    set default route to ideas#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # set 'homepage' to controller#action
+  root 'ideas#index'
+  
 end
