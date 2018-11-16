78a053cc6a1da7b5600f0be32447f4d7756ae556
Mav <mavericktesini@gmail.com>
2018-11-16 04:56:39 -0500

    layout create idea spec

---
+++ spec/features/create_idea_spec.rb
@@ -0,0 +1,14 @@
+require 'rails-helper'
+
+RSpec.describe 'home page', type: :request do
+  it 'displays a newly created idea' do
+    idea = FactoryBot.create(:idea, name: 'Best Idea', description: 'The best idea, ever!', picture: 'The best image')
+    visit '/ideas/new'
+    fill_in 'Name', with: 'Best idea'
+    fill_in 'Description', with: 'The best idea, ever!'
+    fill_in 'Picture', with: 'The best image'
+    click_button 'Create Idea'
+
+    expect(page).to have_selector('td', text: 'Best Idea')
+  end
+end
