64f5556f2b34caf3c01b2744e6eb2637dfd5b19a
Mav <mavericktesini@gmail.com>
2018-11-16 04:01:45 -0500

    update to rails helper require and greens on linux

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe Idea, type: :model do 
-  it "has a name" do 
-    idea  = Idea.create!(name: "My Awesome Idea Name") #instantiate an instance
-    expect(idea.name).to eq("My Awesome Idea Name") #expect the arg to exist and be the assigned name
+RSpec.describe Idea, type: :model do
+  it 'has a name' do
+    idea = Idea.create!(name: 'My Awesome Idea Name') # instantiate an instance
+    expect(idea.name).to eq('My Awesome Idea Name') # expect the arg to exist and be the assigned name
   end
-end 
+end
