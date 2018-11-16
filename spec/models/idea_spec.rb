0586e3568b963f00b0f5276cace3b0dce18d121d
Mav <mavericktesini@gmail.com>
2018-11-16 04:30:31 -0500

    add test for idea picture

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Idea, type: :model do
     idea = Idea.create!(description: 'Description of an Awesome Idea')
     expect(idea.description).to eq('Description of an Awesome Idea')
   end
+
+  it 'has a picture reference' do
+    idea = Idea.new(picture: "Pic Link")
+    expect(idea.picture).to eq('Pic Link')
+  end 
+  
 end
